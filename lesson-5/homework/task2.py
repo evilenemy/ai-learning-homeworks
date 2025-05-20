3284e6e8fad9c06e0673c14cec403d7aa387a40b
Olimov Amirbek <olimovamirbek55@gmail.com>
2025-05-20 16:01:40 +0500

    lesson-5 finished

---
+++ lesson-5/homework/task2.py
@@ -0,0 +1,8 @@
+# def invest(amount, rate, years=1):
+#   total = amount
+#   for i in range(years):
+#     total = total + total * rate
+#     print(f"year {i+1}: ${total:.2f}")
+
+
+# invest(100, 0.05, 4)
---
+++ lesson-5/homework/task3.py
@@ -0,0 +1,12 @@
+# try:
+#   n = int(input("Enter a positive integer: "))
+#   if n <= 0:
+#     print("Enter positive and not 0 value!")
+#     exit()
+#   for i in range(1, n + 1):
+#     if n % i == 0:
+#       print(f"{i} is a factor of {n}")
+# except ValueError:
+#   print("Please enter number!")
+# except Exception as e:
+#   print(e)
