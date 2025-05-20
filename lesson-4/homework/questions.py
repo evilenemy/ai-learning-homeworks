3317bd166d287357877350697b887c5f90f7f838
Olimov Amirbek <olimovamirbek55@gmail.com>
2025-05-20 14:50:20 +0500

    lesson-4 finished

---
+++ lesson-4/homework/questions.py
@@ -0,0 +1,11 @@
+# 1. What is the difference between the continue and break statements in Python?
+# 2. Can you explain the difference between for loop and while loop?
+# 3. How would you implement a nested for loop system? Provide an example
+
+# 1. "continue" skip after code and next to next iteration, "break" stops loop
+# 2. for loop works until iterable object will have no next object, while lopp works until condition would be False
+
+# 3
+# for i in range(10):
+#     for j in range(i, 10):
+#         print(i, j)
